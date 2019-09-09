7bc41b1d492a6a359d70e18dc3a7cdbff1e65f08
Shvetc Vitaliy <iamdobro@gmai.com>
2019-09-09 18:19:53 +0300

    Initial commit

---
+++ app/src/main/java/com/example/beatbox/Sound.java
@@ -0,0 +1,30 @@
+package com.example.beatbox;
+
+public class Sound {
+    private String mAssetPath;
+    private String mName;
+    private Integer mSoundId;
+
+
+
+    public Sound(String assetPath) {
+        mAssetPath = assetPath;
+        String[] components = assetPath.split("/");
+        String filename = components[components.length - 1];
+        mName = filename.replace(".wav", "");
+    }
+    public String getmAssetPath() {
+        return mAssetPath;
+    }
+
+    public String getmName() {
+        return mName;
+    }
+    public Integer getmSoundId() {
+        return mSoundId;
+    }
+
+    public void setmSoundId(Integer mSoundId) {
+        this.mSoundId = mSoundId;
+    }
+}
